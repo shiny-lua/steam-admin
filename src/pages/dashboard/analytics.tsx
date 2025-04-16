a78fa0df2251bedf2d49b7692820ca84aeb05aac
shiny-lua <sol.trx.bf@gmail.com>
2025-04-16 04:47:11 +0100

    update: analytics

--- src/pages/dashboard/analytics.tsx
+++ src/pages/dashboard/analytics.tsx
@@ -100,26 +100,6 @@ export default function GeneralAnalyticsPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AnalyticsConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chart={{
-                series: [
-                  { label: 'Italy', value: 400 },
-                  { label: 'Japan', value: 430 },
-                  { label: 'China', value: 448 },
-                  { label: 'Canada', value: 470 },
-                  { label: 'France', value: 540 },
-                  { label: 'Germany', value: 580 },
-                  { label: 'South Korea', value: 690 },
-                  { label: 'Netherlands', value: 1100 },
-                  { label: 'United States', value: 1200 },
-                  { label: 'United Kingdom', value: 1380 },
-                ],
-              }}
-            />
-          </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
             <AnalyticsOrderTimeline title="Order Timeline" list={_analyticOrderTimeline} />
